4aadaf5a326a5dab24b171db10bc63315938dcba
Chirag Pandit <chirag.pandit@here.com>
2019-02-23 00:05:51 +0530

    Removing files

--- web/includesPage/hesders_sidebars/_subCategoryList.jsp
+++
@@ -1,8 +0,0 @@
-<%-- 
-    Document   : _subCategoryList
-    Created on : 15 Nov, 2012, 11:26:49 PM
-    Author     : chirag
---%>
-
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-
--- web/js/product.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function (){
-    $('#selectMenuCategory').click (function (){
-        var category = $(this).attr("value");
-        //alert (" "+category);
-        $.get('getSubCategory', { category:category }, function (data){
-                $('#selectMenuSubCategory').html(data);
-        })
-    });
-});
