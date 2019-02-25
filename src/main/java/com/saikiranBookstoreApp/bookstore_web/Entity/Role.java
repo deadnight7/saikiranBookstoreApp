ab7cbd191c91ba7a93c71561352503bd09f7a8c5
Chirag Pandit <chirag.pandit@here.com>
2019-02-25 09:47:43 +0530

    Staging changes

---
+++ src/main/java/com/saikiranBookstoreApp/bookstore_web/Entity/Role.java
@@ -0,0 +1,43 @@
+package com.saikiranBookstoreApp.bookstore_web.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set <User> users;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
--- src/main/java/com/saikiranBookstoreApp/bookstore_web/Security/SecurityConfiguration.java
+++ src/main/java/com/saikiranBookstoreApp/bookstore_web/Security/SecurityConfiguration.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class SecurityConfiguration{
-    
+
 }
 //@Configuration
 //@EnableWebSecurity
